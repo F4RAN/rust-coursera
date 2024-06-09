85bb084cf290e7cf9aab7525c22a63d6a78cc155
F4RAN <parsa.hassani30@gmail.com>
2024-06-09 09:19:02 +0000

    Second assignment

---
+++ main.rs
@@ -0,0 +1,5 @@
+// Give me a function named `main` that takes no arguments and returns nothing
+fn main() {
+    // Print the string "Hello, world!" to the console
+    println!("Hello, world!");
+}
